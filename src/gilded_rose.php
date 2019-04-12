a3531bf98a011146ba2bae30c01778a47f714421
justinabur <justina.burm@gmail.com>
2019-04-12 07:58:32 +0300

    added more tests for specific situations, improved the code

--- src/gilded_rose.php
+++ src/gilded_rose.php
@@ -8,48 +8,75 @@ class GildedRose {
         $this->items = $items;
     }
 
+
+    function update_quality() {
+        /** Specific increments or decrements in value */
+        $valAgedBrie = +1;
+        $valNormal = -1;
+        $valConjured = $valNormal * 2;      // "Conjured" items degrade in Quality twice as fast as normal items
+        $valPasses = array (                // Array of "Backstage Passes"
+            ['day'=>0, 'value'=>0],         // The first array's value is a bit different than others. Because the item's value does not decrease by the value specified, but becomes equal
+            ['day'=>5, 'value'=>3],
+            ['day'=>10, 'value'=>2]         // If you add more 'rules', please add more rules to the code below as well.
+            );
+
+        /** Array of Legendary items to add more specific items if needed */
+        $legendaryItems = array (
+            'Sulfuras, Hand of Ragnaros'
+        );
+
+
+        //Going through all items in the store at the end od the day
+        foreach ( $this->items as $item ) {
+            $itemName = $item->name;
+            if ( in_array( $itemName, $legendaryItems ) ) {
+                // Leave as it is. It's a legendary item.
+            }
+            else {
+                $itemName = ucwords($item->name); // Converts the first character of each word in a string to uppercase to avoid mistakes in not legendary titles
+                $this->end_the_day( $item ); // Days make impact to all other items (except legendary ones)
+
+                if ( $itemName == 'Aged Brie' ) {                          // Aged Brie
+                    $this->change_quality($item, $valAgedBrie);
+                }
+
+                else {
+                    $arrItemName = explode(' ', $itemName, 3); // Getting the first two words of itemTitle in array
+
+                    if ($arrItemName[0] == 'Conjured') {                      // Conjured Items
+                        $this->change_quality($item, $valConjured);
+                    }
+
+                    else if ($arrItemName[0] == 'Backstage' && $arrItemName[1] == 'Passes') {       // Backstage Passes to Concerts
+                            if              ( $item->sell_in <  $valPasses[ 0 ]['day'] ) { $item->quality =             $valPasses[ 0 ]['value']; }
+                            else if         ( $item->sell_in <= $valPasses[ 1 ]['day'] ) { $this->change_quality($item, $valPasses[ 1 ]['value']); }
+                                 else if    ( $item->sell_in <= $valPasses[ 2 ]['day'] ) { $this->change_quality($item, $valPasses[ 2 ]['value']); }
+                                      else  { $this->change_quality($item, +1); }
+
+                    } else { $this->change_quality($item, $valNormal); }        // All other Normal Items
+                }
+            }
+        }
+    }
+
     /** Function to decrease the amount of days left to sell for particular item */
     function end_the_day($item){
-//        if ($item->sell_in == 0) return;
         $item->sell_in -= 1;
     }
 
     /** Function to change the quality of an item by the amount given */
     function change_quality($item, $change) {
+        if ($change < 0 && $item->sell_in < 0 ) { $change *= 2; }   // Once the sell by date has passed, Quality degrades twice as fast
         $item->quality += $change;
-    }
 
-    function update_quality() {
-
-        foreach ($this->items as $item) {
-            if ($item->name == 'Sulfuras, Hand of Ragnaros') {
-                // Leave as it is. It's a legendary item.
-            }
-            else {
-                $this->end_the_day($item); // Days make impact to all other items (except legendary ones)
-
-                if ($item->name == 'Aged Brie') {
-                    $this->change_quality($item, +1);
-                }
-
-                else if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if              ( $item->sell_in < 0 ) { $item->quality = 0; }
-                    else if         ( $item->sell_in <= 5 ) { $this->change_quality($item, +3); }
-                         else if    ( $item->sell_in <= 10 ) { $this->change_quality($item, +2); }
-                              else  { $this->change_quality($item, +1); }
-                }
-
-                else if ($item->name == 'Conjured Mana Cake') {
-                    $this->change_quality($item, -2);
-                }
-
-                else {
-                    $this->change_quality($item, -1);
-                }
-            }
-
+        $temporaryQuality = $item->quality;                         // The Quality of an item is never more than 50
+        if ($temporaryQuality < 0) {
+            $item->quality = 0;
+        } else if ($temporaryQuality > 50) {
+            $item->quality = 50;
         }
     }
+
 }
 
 // Do not change anything here because of Goblin !!
--- test/gilded_rose_test.php
+++ test/gilded_rose_test.php
@@ -23,8 +23,29 @@ class GildedRoseTest extends TestCase {
             $expectedItem = new Item('Aged Brie', 1, 1);
             $this->assertEquals($expectedItem, $items[0], 'Aged Brie "sell_in" should decrease by one and "quality" increase by one.');
         }
+        function testBrieItem2() {
+            $items = array(new Item('Aged Brie', 2, 50));
+            $gildedRose = new GildedRose($items);
+            $gildedRose->update_quality();
+            $expectedItem = new Item('Aged Brie', 1, 50);
+            $this->assertEquals($expectedItem, $items[0], 'Aged Brie "sell_in" should decrease by one and "quality" increase by one. But quality is never more than 50');
+        }
+        function testBrieItem3() {
+            $items = array(new Item('Aged Brie', -1, 49));
+            $gildedRose = new GildedRose($items);
+            $gildedRose->update_quality();
+            $expectedItem = new Item('Aged Brie', -2, 50);
+            $this->assertEquals($expectedItem, $items[0], 'Aged Brie "sell_in" should decrease by one and "quality" increase by one.');
+        }
 
         /** Backstage Passes */
+        function testBackstagePasses0() {
+            $items = array(new Item('Backstage passes to a TAFKAL80ETC concert', 0, 49));
+            $gildedRose = new GildedRose($items);
+            $gildedRose->update_quality();
+            $expectedItem = new Item('Backstage passes to a TAFKAL80ETC concert', -1, 0);
+            $this->assertEquals($expectedItem, $items[0], '"Backstage passes" quality should drop to 0 after the concert');
+        }
         function testBackstagePasses1() {
             $items = array(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
             $gildedRose = new GildedRose($items);
@@ -33,36 +54,36 @@ class GildedRoseTest extends TestCase {
             $this->assertEquals($expectedItem, $items[0], 'Backstage passes quality should increase by 1, when it\'s more than 10 days.');
         }
         function testBackstagePasses2() {
-            $items = array(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49));
+            $items = array(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 30));
             $gildedRose = new GildedRose($items);
             $gildedRose->update_quality();
-            $expectedItem = new Item('Backstage passes to a TAFKAL80ETC concert', 9, 51);
+            $expectedItem = new Item('Backstage passes to a TAFKAL80ETC concert', 9, 32);
             $this->assertEquals($expectedItem, $items[0], '"Backstage passes" quality should increase by 2, when it\'s less than 10 && more than 5 days left.');
         }
         function testBackstagePasses3() {
-            $items = array(new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49));
+            $items = array(new Item('Backstage passes to a TAFKAL80ETC concert', 5, 30));
             $gildedRose = new GildedRose($items);
             $gildedRose->update_quality();
-            $expectedItem = new Item('Backstage passes to a TAFKAL80ETC concert', 4, 52);
+            $expectedItem = new Item('Backstage passes to a TAFKAL80ETC concert', 4, 33);
             $this->assertEquals($expectedItem, $items[0], '"Backstage passes" quality should increase by 3, when it\'s less than 5 days left.');
         }
-        function testBackstagePasses0() {
-            $items = array(new Item('Backstage passes to a TAFKAL80ETC concert', 0, 49));
+        function testBackstagePasses4() {
+            $items = array(new Item('Backstage passes to some other random concert', 5, 30));
             $gildedRose = new GildedRose($items);
             $gildedRose->update_quality();
-            $expectedItem = new Item('Backstage passes to a TAFKAL80ETC concert', -1, 0);
-            $this->assertEquals($expectedItem, $items[0], '"Backstage passes" quality should drop to 0 after the concert');
+            $expectedItem = new Item('Backstage passes to some other random concert', 4, 33);
+            $this->assertEquals($expectedItem, $items[0], '"Backstage passes" quality should increase by 3, when it\'s less than 5 days left. Also, should recognise it is "backstage passes"');
         }
 
-        /** Sulfuras Item */
-        function testSulfurasItem() {
+        /** Legendary Item */
+        function testLegendaryItem() {
             $items = array(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
             $gildedRose = new GildedRose($items);
             $gildedRose->update_quality();
             $expectedItem = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
             $this->assertEquals($expectedItem, $items[0], '"Sulfuras" item never has to be sold or decreases in Quality');
         }
-        function testSulfurasItem2() {
+        function testLegendaryItem2() {
             $items = array(new Item('Sulfuras, Hand of Ragnaros', -1, 80));
             $gildedRose = new GildedRose($items);
             $gildedRose->update_quality();
@@ -78,11 +99,28 @@ class GildedRoseTest extends TestCase {
             $expectedItem = new Item('Conjured Mana Cake', 2, 4);
             $this->assertEquals($expectedItem, $items[0], '"Conjured" items degrade in Quality twice as fast as normal items');
         }
+        function testConjuredItem2() {
+            $items = array(new Item('Conjured Flower', 3, 6));
+            $gildedRose = new GildedRose($items);
+            $gildedRose->update_quality();
+            $expectedItem = new Item('Conjured Flower', 2, 4);
+            $this->assertEquals($expectedItem, $items[0], '"Conjured" items degrade in Quality twice as fast as normal items');
+        }
 
-        /** Yet to add:
-         *  Once the sell by date has passed, Quality degrades twice as fast
-         *  The Quality of an item is never negative
-         *  The Quality of an item is never more than 50
-         */
+        /** Test Passed Date */
+        function testPassedDate1() {
+            $items = array(new Item('Conjured Mana Cake', 0, 6));
+            $gildedRose = new GildedRose($items);
+            $gildedRose->update_quality();
+            $expectedItem = new Item('Conjured Mana Cake', -1, 2);
+            $this->assertEquals($expectedItem, $items[0], 'Once the sell by date has passed, Quality degrades twice as fast. Plus, "Conjured" items degrade in Quality twice as fast as normal items');
+        }
+        function testPassedDate2() {
+            $items = array(new Item('Simple Item', -3, 6));
+            $gildedRose = new GildedRose($items);
+            $gildedRose->update_quality();
+            $expectedItem = new Item('Simple Item', -4, 4);
+            $this->assertEquals($expectedItem, $items[0], 'Once the sell by date has passed, Quality degrades twice as fast. Plus');
+        }
 }
 
